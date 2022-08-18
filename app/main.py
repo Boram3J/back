b3f49484cb0c36463b88c0b6578332e90e4a6808
KTMMMM <110892552+KTMMMM@users.noreply.github.com>
2022-08-18 18:24:34 +0900

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,12 +5,34 @@ from fastapi import FastAPI, File, UploadFile
 from PIL import Image
 from starlette.responses import StreamingResponse
 
+from ocr_port import run_ocr , papago
+
 app = FastAPI()
 
+@app.get("/")
+def root():
+    print("ajsdhldhad")
+    return {"message": "Hello World"}
 
-@app.post("/image")
+
+@app.post("/api/image")
 def get_image(file: UploadFile = File(...)):
     image = np.array(Image.open(file.file))
     # process image
     # convert to png bytes
     return StreamingResponse(io.BytesIO(image.tobytes()), media_type="image/png")
+
+
+@app.post("/api/translate")
+def translate(file: UploadFile = File(...)):
+    image = np.array(Image.open(file.file))
+    # process image
+    
+    #detection
+    #recognition
+    run_ocr(img=image)
+    #papago
+    papago()
+    #reult
+    # convert to png bytes
+    return StreamingResponse(io.BytesIO(result.tobytes()), media_type="image/png")
